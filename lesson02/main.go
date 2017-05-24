85c081875a3f3d9eb43f5ad57169175d54be36e4
Grigory Koksharov <griddic@mail.ru>
2017-05-25 01:22:53 +0300

    спать пора

--- lesson02/main.go
+++ lesson02/main.go
@@ -1,9 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+var suffixes = []string{"B", "KB", "MB", "GB"}
+
+func main() {
+	fmt.Println(humanize(100))
+	fmt.Println(humanize(12345))
 }
 
+
 func humanize(n uint64) string {
-	return ""
+	fmt.Println(n)
+	len := int(math.Floor(math.Log10(float64(n))))
+	var devider uint64 = 1
+	if len > 2 {
+		devider = uint64(math.Pow10(len - 2))
+	}
+	to_print := n / devider
+	point_location := (len - 2) % 3
+	suffix_index := len / 3
+	to_print_float := float64(to_print) / float64(math.Pow10(point_location))
+	fmt.Println(len, devider, to_print, to_print_float,
+		point_location, suffixes[suffix_index])
+
+	return fmt.Sprint(to_print_float) + suffixes[suffix_index]
 }
--- lesson02/main_test.go
+++ lesson02/main_test.go
@@ -4,3 +4,6 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
+func Test_Byte(t *testing.T) {
+	assert.Equal(t, "5B", humanize(5))
+}
