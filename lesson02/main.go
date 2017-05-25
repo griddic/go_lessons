c95ca43a8acba66c49621a4fcdb8b4ad4045c8bf
grigorii.koksharov <griddic@mail.ru>
2017-05-25 20:13:16 +0300

    simple refactoring

--- lesson02/main.go
+++ lesson02/main.go
@@ -7,32 +7,23 @@ import (
 
 var suffixes = []string{"B", "KB", "MB", "GB"}
 
-func main() {
-	fmt.Println(humanize(5))
-	fmt.Println(humanize(100))
-	fmt.Println(humanize(12345))
-	fmt.Println(humanize(2100000))
-}
-
+func main() {}
 
 func humanize(n uint64) string {
-	//fmt.Println(n)
-	len := uint(math.Floor(math.Log10(float64(n))))
-	if len > 11 {
-		len = 11
+	length := uint(math.Floor(math.Log10(float64(n))))
+	if length > 11 {
+		length = 11
 	}
-	var devider uint64 = 1
-	var point_location uint = 0
-	if len > 2 {
-		devider = uint64(math.Pow10(int(len) - 2))
-		point_location = (5 - len + 3 * len) % 3
-	}
-	to_print := n / devider
 
-	suffix_index := len / 3
-	to_print_float := float64(to_print) / float64(math.Pow10(int(point_location)))
-	//fmt.Println(len, devider, to_print, to_print_float,
-	//	point_location, suffixes[suffix_index])
+	var divider uint64 = 1
+	var point_location uint = 0
+	if length > 2 {
+		divider = uint64(math.Pow10(int(length) - 2))
+		point_location = (2 * (length + 1)) % 3
+	}
+
+	suffix_index := length / 3
+	to_print_float := float64(n /divider) / float64(math.Pow10(int(point_location)))
 
 	return fmt.Sprint(to_print_float) + suffixes[suffix_index]
 }
